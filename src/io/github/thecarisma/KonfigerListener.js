6cb0006159ea37e68672788fb199473f3956e8bd
Azeez Adewale <azeezadewale98@gmail.com>
2020-02-17 16:35:41 +0100

    creates the KonfigerListener and it test

--- src/io/github/thecarisma/KonfigerListener.js
+++ src/io/github/thecarisma/KonfigerListener.js
@@ -8,34 +8,36 @@
 
 var KonfigerListener = function() {
     if (this.constructor === KonfigerListener) {
-      throw new Error("Can't instantiate abstract class!");
+      throw new Error("Can't instantiate abstract class! - KonfigerListener");
     }
 };
 
 /**
  @abstract
  */
-KonfigerListener.prototype.onValueAdded(object, key) = function() {
+KonfigerListener.prototype.onValueAdded = function(object, key) {
     throw new Error("Abstract method!, simply implements the abstract method - onValueAdded");
 }
 
 /**
  @abstract
  */
-KonfigerListener.prototype.onChange(object) = function() {
+KonfigerListener.prototype.onChange = function(object) {
     throw new Error("Abstract method!, simply implements the abstract method - onChange");
 }
 
 /**
  @abstract
  */
-KonfigerListener.prototype.onValueChanged(object, key, oldValue, newValue) = function() {
+KonfigerListener.prototype.onValueChanged = function(object, key, oldValue, newValue) {
     throw new Error("Abstract method!, simply implements the abstract method - onValueChanged");
 }
 
 /**
  @abstract
  */
-KonfigerListener.prototype.onValueRemoved(object, konfigerObject) = function() {
+KonfigerListener.prototype.onValueRemoved = function(object, konfigerObject) {
     throw new Error("Abstract method!, simply implements the abstract method - onValueRemoved");
 }
+
+module.exports = KonfigerListener;
--- test/KonfigerListenerTest.js
+++ test/KonfigerListenerTest.js
@@ -1,3 +1,19 @@
 const KonfigerListener = require("../src/io/github/thecarisma/KonfigerListener.js");
 
-var konfigerListener = new KonfigerListener()
+
+try {
+    var konfigerListener = new KonfigerListener()
+} catch (err) {
+    console.log(err.message)
+}
+
+var Listener = function() {
+    console.log("booyah inititialize it, abstract working")
+};
+Listener.prototype = Object.create(KonfigerListener.prototype)
+Listener.prototype.onValueAdded = function(object, key) {
+    console.log("onValueAdded: " + key)
+}
+
+var lisner = new Listener()
+lisner.onValueAdded(null, "Key")
