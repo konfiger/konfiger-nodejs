9a2a7f91364309fa64a3eba8a58c9563e22b8567
Azeez Adewale <azeezadewale98@gmail.com>
2020-02-17 15:01:02 +0100

    creates the KonfigerObject and it test

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const KonfigerObject_ = require("./src/io/github/thecarisma/KonfigerObject.js");
 const Konfiger_ = require("./src/io/github/thecarisma/Konfiger.js");
 const KonfigerStream_ = require("./src/io/github/thecarisma/KonfigerStream.js");
-const KonfigerListener = require("./src/io/github/thecarisma/KonfigerListener.js");
+const KonfigerListener_ = require("./src/io/github/thecarisma/KonfigerListener.js");
 
 module.exports = {
     KonfigerObject: KonfigerObject_,
