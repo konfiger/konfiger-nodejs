813299c0886bbd97ab24b785b36cda927a2dda54
Azeez Adewale <azeezadewale98@gmail.com>
2020-02-18 12:04:26 +0100

    created Konfiger and it tests

--- src/io/github/thecarisma/Konfiger.js
+++ src/io/github/thecarisma/Konfiger.js
@@ -6,8 +6,32 @@
  *
  */
  
+const konfigerUtil = require("./KonfigerUtil.js")
+const KonfigerObject = require("./KonfigerObject.js")
+const KonfigerStream = require("./KonfigerStream.js")
+ 
 const MAX_CAPACITY = Number.MAX_SAFE_INTEGER - 1
 
+function fromFile(filePath, delimeter, seperator) {
+    return fromStream((new KonfigerStream('test/test.config.ini', delimeter, seperator)))
+}
+
+function fromString(filePath, delimeter, seperator) {
+    
+}
+
+function fromStream(konfigerStream) {
+    const konfiger = new Konfiger()
+    konfiger.createdFromStream = true
+    konfiger.stream = konfigerStream
+    return konfiger
+}
+
+function Konfiger() {
+    this.text = "testing"
+}
+
 module.exports = { 
-    MAX_CAPACITY
+    MAX_CAPACITY,
+    fromFile
 }
--- test/KonfigerTest.js
+++ test/KonfigerTest.js
@@ -1,3 +1,6 @@
 const Konfiger = require("../src/io/github/thecarisma/Konfiger.js");
 
 console.log(Konfiger.MAX_CAPACITY)
+
+var konfiger = Konfiger.fromFile()
+console.log(konfiger.text)
