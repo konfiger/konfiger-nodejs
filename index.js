7a901bedd7b54ccd4f6d7e4a88886f453cd27e3b
Adewale Azeez <azeezadewale98@gmail.com>
2020-02-20 21:27:55 +0100

    remove useless KonfigerObject class

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-const KonfigerObject_ = require("./src/io/github/thecarisma/KonfigerObject.js")
 const Konfiger_ = require("./src/io/github/thecarisma/Konfiger.js")
 const KonfigerStream_ = require("./src/io/github/thecarisma/KonfigerStream.js")
 const KonfigerUtil_ = require("./src/io/github/thecarisma/KonfigerUtil.js")
 
 module.exports = {
-    KonfigerObject: KonfigerObject_,
     Konfiger: Konfiger_,
     KonfigerStream: KonfigerStream_,
     KonfigerUtil: KonfigerUtil_
